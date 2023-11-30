f41b339430f44baa6adb25fbff4191a7bd400bb3
Linjie Xu (FA Talent) <v-linjiexu@microsoft.com>
2023-11-30 15:02:13 +0800

    make it a package

--- edp/diffusion/trainer.py
+++ edp/diffusion/trainer.py
@@ -57,43 +57,6 @@ from edp.utilities.utils import (
 )
 from edp.viskit.logging import logger, setup_logger
 
-FLAGS_DEF = define_flags_with_default(
-  #algo="DiffQL",
-  # algo="DiffusionQL",
-  type="model-free",
-  env="walker2d-medium-replay-v2",
-  dataset='d4rl',
-  rl_unplugged_task_class='control_suite',
-  max_traj_length=1000,
-  save_model=False,
-  seed=42,
-  batch_size=256,
-  reward_scale=1,
-  reward_bias=0,
-  clip_action=0.999,
-  encoder_arch="64-64",
-  policy_arch="256-256-256",
-  qf_arch="256-256-256",
-  orthogonal_init=False,
-  policy_log_std_multiplier=1.0,
-  policy_log_std_offset=-1.0,
-  algo_cfg=DiffusionQL.get_default_config(),
-  n_epochs=2000,
-  n_train_step_per_epoch=1000,
-  eval_period=10,
-  eval_n_trajs=10,
-  logging=WandBLogger.get_default_config(),
-  qf_layer_norm=False,
-  policy_layer_norm=False,
-  activation="mish",
-  obs_norm=False,
-  act_method='',
-  sample_method='ddpm',
-  policy_temp=1.0,
-  norm_reward=False,
-)
-
-
 class SamplerPolicy(object):
 
   def __init__(
@@ -214,7 +177,7 @@ class SamplerPolicy(object):
 
 class DiffusionTrainer:
 
-  def __init__(self):
+  def __init__(self, FLAGS_DEF):
     self._cfgs = absl.flags.FLAGS
     self._algo = DiffusionQL
     self._algo_type = 'DiffusionQL'
@@ -230,7 +193,7 @@ class DiffusionTrainer:
 
     self._act_fn = act_fn
 
-    self._variant = get_user_flags(self._cfgs, FLAGS_DEF)
+    self._variant = get_user_flags(self._cfgs)
     for k, v in self._cfgs.algo_cfg.items():
       self._variant[f"algo.{k}"] = v
 
